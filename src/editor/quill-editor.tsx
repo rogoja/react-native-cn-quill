520a706045986ef2d53ff2fc3ebf4a75f6ad7492
rogoja <vasilyrogoja@gmail.com>
2024-02-28 21:41:47 +0200

    fix: fixed readOnly

--- src/editor/quill-editor.tsx
+++ src/editor/quill-editor.tsx
@@ -59,6 +59,7 @@ export interface EditorProps {
   onBlur?: () => void;
   onFocus?: () => void;
   customJS?: string;
+  readOnly?: boolean;
 }
 
 export default class QuillEditor extends React.Component<
@@ -135,6 +136,7 @@ export default class QuillEditor extends React.Component<
       customStyles = [],
       defaultFontFamily = undefined,
       customJS = '',
+      readOnly = false,
     } = this.props;
 
     return createHtml({
@@ -155,6 +157,7 @@ export default class QuillEditor extends React.Component<
       placeholderColor: theme.placeholder,
       customStyles,
       customJS,
+      readOnly,
     });
   };
 
